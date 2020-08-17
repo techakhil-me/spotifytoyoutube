25687b04e0488d957f993c440cb4fd8d8e98ae91
TechAkhil <43501564+techakhilc47@users.noreply.github.com>
2020-08-17 16:44:54 +0530

    added exceptions

--- main/views.py
+++ main/views.py
@@ -13,24 +13,30 @@ def getLink(request):
     if request.method == "POST":
         link = request.POST.get('link')
         link = createLink(link)
-    return render(request, "index.html", {'link': link})
+    if link:
+        return render(request, "index.html", {'link': link})
+    else: 
+        return render(request, "index.html",{'link': 'InvalidLink'})
 
 
 def createLink(LINK):
-    from spotipy.oauth2 import SpotifyClientCredentials
-    import spotipy
-    from youtube_search import YoutubeSearch
+    try:
+        from spotipy.oauth2 import SpotifyClientCredentials
+        import spotipy
+        from youtube_search import YoutubeSearch
 
 
-    client_credentials_manager = SpotifyClientCredentials(client_id='e5d66c188ef64dd89afa4d13f9555411',
-                    client_secret='d070988d7bd5479a9e0818fa23839544')
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    PlaylistLink = "http://www.youtube.com/watch_videos?video_ids="
-    for i in (sp.playlist_tracks(LINK)['items']):
-        try:
-            song = i['track']['name'] + i['track']['artists'][0]['name']
-            PlaylistLink += (YoutubeSearch(song, max_results=1).to_dict()
-                            )[0]['id'] + ','
-        except:
-            pass
-    return PlaylistLink
+        client_credentials_manager = SpotifyClientCredentials(client_id='e5d66c188ef64dd89afa4d13f9555411',
+                        client_secret='d070988d7bd5479a9e0818fa23839544')
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        PlaylistLink = "http://www.youtube.com/watch_videos?video_ids="
+        for i in (sp.playlist_tracks(LINK)['items']):
+            try:
+                song = i['track']['name'] + i['track']['artists'][0]['name']
+                PlaylistLink += (YoutubeSearch(song, max_results=1).to_dict()
+                                )[0]['id'] + ','
+            except:
+                pass
+        return PlaylistLink
+    except:
+        return False
--- static/main.js
+++ static/main.js
@@ -1,4 +1,16 @@
-function submit() {
+function check() {
+  var url =  document.forms["urlform"]["link"].value;
+  if (url.includes("open.spotify.com")) {
+    document.getElementById("submit").remove();
+    return "location.href='{%  url 'getlink' %}'";
+  }else{
+    alert("INVALID LINK !!");
+    return false;
+  }
+}
+function submitbtn(){  
+  if (check()){
   document.getElementById("submit").remove();
   document.getElementById("loading").classList.remove("hidden");
+  }
 }
